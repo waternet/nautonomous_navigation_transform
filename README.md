649ee9b436f8ecf5017d8d623228d5d9c2917885
jesse <jessebeekman@gmail.com>
2017-03-28 14:25:53 +0200

    readme added, imu boot error resolved

---
+++ README.md
@@ -0,0 +1,31 @@
+# nautonomous navigation transform {#nautonomous_navigation_transform}
+Combines odom+imu to create a transform for /tf.
+
+
+nautonomous_transforms_node.cpp
+
+##Nodes
+nautonomous_transforms_node
+
+/camera_link_broadcaster <br />
+/footprint_link_broadcaster <br />
+/gps_link_broadcaster <br />
+/imu_link_broadcaster <br />
+/odom_combined_broadcaster
+
+##Topics
+###Subscribe
+/gps/odom
+<br />
+/imu/razor
+
+###Publish
+/tf
+
+## Files
+[Src](../../doxygen_nautonomous/html/dir_5f483fb73a60372554ae375b13d59e7b.html)
+
+##Overview
+![launch_nautonomous_transforms.launch](../images/launch_nautonomous_transforms.png)
+<br />
+![legenda](../images/legenda.png)
--- src/nautonomous_transforms_node.cpp
+++ src/nautonomous_transforms_node.cpp
@@ -5,18 +5,31 @@
 
 tf::Quaternion quaternion;
 
-//Subscribe to the IMU topic and extract the orientation
+/**
+ *\brief Subscribe to the IMU topic and extract the orientation
+ *\param sensor_msgs::Imu msg
+ *\return
+ */
 void imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
     quaternion = tf::Quaternion(0,0,msg->orientation.z+0.327,msg->orientation.w-0.268); //TODO remove MAGIC numbers
 }
 
-//Subscribe to the pose topic and extract the rotation and position
+/**
+ *\brief Subscribe to the pose topic and extract the rotation and position. Send transform(gps+imu) to TransformBroadcaster 
+ *\param nav_msgs::Odometry msg
+ *\return
+ */
 void poseCallback(const nav_msgs::Odometry::ConstPtr& msg){
-    static tf::TransformBroadcaster br;
-    tf::Transform transform;
-    transform.setOrigin(tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, 0.0) );
-    transform.setRotation(quaternion);
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom_combined", "map"));
+    //ROS_INFO("Quaternion: %.2f", );
+    
+    if(quaternion.getW() != 0.000 || quaternion.getZ() != 0.000){
+        static tf::TransformBroadcaster br;
+        tf::Transform transform;
+        transform.setOrigin(tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, 0.0) );
+        transform.setRotation(quaternion);
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom_combined", "map"));
+    }
+    
 }
 
 // Is now done by the tf nodes from the launch file.
@@ -56,11 +69,17 @@ void poseCallback(const nav_msgs::Odometry::ConstPtr& msg){
 //   }
 //
 
+
+/*
+ *\brief Main for nautonomous navigation transform. Subscribes to GPS and IMU topic.
+ *\param
+ *\return
+ */
 int main(int argc, char** argv){
   ros::init(argc, argv, "nautonomous_tf_publisher");
 
   ros::NodeHandle n;
-  
+
   //Subscribe to GPS and IMU topic
   ros::Subscriber odometrySubscriber = n.subscribe("odom", 100, &poseCallback);
   ros::Subscriber imuSubscriber = n.subscribe("imu", 100, &imuCallback);
